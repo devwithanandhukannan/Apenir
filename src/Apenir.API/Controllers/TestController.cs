c3a003b3796f913c3406141f696b45f99ada5d8f
JeslinCJohn <jeslin.john@appsteamtechnologies.com>
2026-06-24 01:18:29 +0530

    12345:feat - test controller added

---
+++ src/Apenir.API/Controllers/TestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Apenir.API.Controllers;
+
+[ApiController]
+[Route("api/reports/[controller]")]
+public class TestController : ControllerBase
+{
+	[HttpGet]
+	public IActionResult Get() => Ok(new { status = "OK" });
+}
